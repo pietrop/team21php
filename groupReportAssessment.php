9fd8d87ccc5d1fdcf0361f263d00ea1fe70a547a
kartik <kartik.joshi.14@ucl.ac.uk>
2015-02-13 13:47:13 +0000

    made changes to groupreportassessment files. still work in progress.

--- groupReportAssessment.php
+++ groupReportAssessment.php
@@ -6,30 +6,30 @@ class GroupReportAssessment {
 	
 	var $groupID;
 	var $reportID;
-function
-	 __construct($groupID, $reportID){
-		$this->groupID = $groupID;
-		$this->reportID = $reportID;
-	}
-
-	function getGroupID(){
-		return $this->groupID;
-	}
-	function getReportID(){
-		return $this->reportID;
-	}
-
-	/**
-	* Generation of SQL query for adding student details to "students" table
-	*/
-	function createInsertQuery(){
-		if ($this->groupID != null){
-			return 'INSERT INTO groupreportassessments()
-		 VALUES ("'DEFAULT', '.$this->groupID.'","'.$this->reportID.'","'.$this->lastName.'","'.$this->password.'")';
-		} else {
-			return null;
+	function
+		 __construct($groupID, $reportID){
+			$this->groupID = $groupID;
+			$this->reportID = $reportID;
 		}
-	}
+
+		function getGroupID(){
+			return $this->groupID;
+		}
+		function getReportID(){
+			return $this->reportID;
+		}
+
+		/**
+		* Generation of SQL query for adding student details to "students" table
+		*/
+		function createInsertQuery(){
+			if ($this->groupID != null){
+				return 'INSERT INTO groupreportassessment(assessmentID,group_ID,report_ID)
+			 VALUES ("'DEFAULT'","'.$this->groupID.'","'.$this->reportID.'")';
+			} else {
+				return null;
+			}
+		}
 }
 
 ?>
