f543f4b0d14284e54c3555cece64fc916a2f3c7a
Asset <achita.n@me.com>
2015-03-04 12:07:10 +0000

    Commiting before pulling

--- login/index.php
+++ login/index.php
@@ -41,11 +41,13 @@
                 </div>
             </div>
             <div class="form-group">
-            	<div class="col-sm-offset-2 col-sm-1">
+            	<div class="row">
+            	<div class="col-lg-offset-2 col-sm-offset-2 col-xs-1 col-sm-1 col-md-1 col-lg-1 ">
                 	<button type="submit" class="btn btn-primary"> Sign in </button>
                 </div>
-                <div class="col-sm-1">
+                <div class="col-xs-offset-1 col-xs-1 col-sm-1 col-md-1 col-lg-1 ">
                 	<a href="../groupstudent/insert.php"><button type="button" class="btn btn-default"> Register </button></a>
+                </div>
                 </div>
             </div>
         </form>
