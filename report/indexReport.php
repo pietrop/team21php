72fb880aedb8244921c8c3a1d58fb03e531c8bba
kartik <kartik.joshi.14@ucl.ac.uk>
2015-03-06 17:53:44 +0000

    fixed indexreport.php

--- report/indexReport.php
+++ report/indexReport.php
@@ -49,7 +49,7 @@
 <!--RETRIEVEING REPORT LIST FROM DATABASE-->
 
 <?php
-
+include "report.php";
 // $conn = new mysqli($hostname,$user,$password);
 // // Check connection
 // if ($conn->connect_error) {
@@ -61,39 +61,38 @@
 $conn = connectToDb();
 $conn->select_db("team21");
 //****END OF CONNECTION PROCEDURE****
-
 //Retrieving students from DB and storing in an Array
-$query = sprintf("SELECT * FROM reports");
-	$showResult = $conn->query($query);
-	while ($row = $showResult->fetch_array(MYSQLI_ASSOC)){
-		$newReport = new Report($row['reportID'], $row['group_ID'], $row['abstract'], $row['review1'],$row['review2']);
-		$reportsArray[] = $newReport;
-	}
-	//DETERMINING STUDENT's GORUP STATUS
-	// $whichGroupQuery = "SELECT students.email, groups.groupID FROM `students` INNER JOIN groups WHERE students.email=groups.student_ID";
-	// $showResult = $conn->query($whichGroupQuery);
-	// while ($row = $showResult->fetch_array(MYSQLI_ASSOC)){
-	// 	$groupsArray[] = $row;
-	// }
-	// ADDING TO HTML TABLE
+$query = "SELECT * FROM reports";
+$showResult = $conn->query($query);
+while ($row = $showResult->fetch_array(MYSQLI_ASSOC)){
+	$newReport = new Report($row['reportID'], $row['group_ID'], $row['abstract'], $row['review1'],$row['review2']);
+	$reportsArray[] = $newReport;
+}
+//DETERMINING STUDENT's GORUP STATUS
+// $whichGroupQuery = "SELECT students.email, groups.groupID FROM `students` INNER JOIN groups WHERE students.email=groups.student_ID";
+// $showResult = $conn->query($whichGroupQuery);
+// while ($row = $showResult->fetch_array(MYSQLI_ASSOC)){
+// 	$groupsArray[] = $row;
+// }
+// ADDING TO HTML TABLE
 
-	foreach($reportsArray as $rep){
-		$groupID = null;
-		foreach($reportsArray as $group){
-			if ($report['report']==$rep->getReportID()){
-				$groupID = $group['groupID'];
-				echo "1";
-			}
+foreach($reportsArray as $rep){
+	$groupID = null;
+	foreach($reportsArray as $group){
+		if ($report['report']==$rep->getReportID()){
+			$groupID = $group['groupID'];
+			echo "1";
 		}
-		echo "<tr>";
-		echo "<td>".$rep->getReportID()."</td>";
-		echo "<td>".$rep->getGroup_ID()."</td>";
-		echo "<td>".$rep->getAbstract()."</td>";
-		echo "<td>".$rep->getReview1()."</td>";
-		echo "<td>".$rep->getReview2()."</td>";
-		// echo '<td><a href="update.php?email='.$rep->getEmail().'&firstName='.$rep->getFirstName().'&lastName='.$rep->getLastName().'&group='.$groupID.'"> Update </a>&nbsp; &nbsp; <a href="delete.php?email='.$stud->getEmail().'"> Delete </a>';
-		echo "</tr>";
 	}
+	echo "<tr>";
+	echo "<td>".$rep->getReportID()."</td>";
+	echo "<td>".$rep->getGroup_ID()."</td>";
+	echo "<td>".$rep->getAbstract()."</td>";
+	echo "<td>".$rep->getReview1()."</td>";
+	echo "<td>".$rep->getReview2()."</td>";
+	// echo '<td><a href="update.php?email='.$rep->getEmail().'&firstName='.$rep->getFirstName().'&lastName='.$rep->getLastName().'&group='.$groupID.'"> Update </a>&nbsp; &nbsp; <a href="delete.php?email='.$stud->getEmail().'"> Delete </a>';
+	echo "</tr>";
+}
 ?>
 
 </table>
