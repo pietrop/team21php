ed2b8d9d21d455ea5fc78a04cada7ddfbe5adb60
pietrop <pietro.passarelli@gmail.com>
2015-03-11 14:59:55 +0000

    changed user hompeage

--- homePage/index.php
+++ homePage/index.php
@@ -5,7 +5,7 @@
      
     <div class="container">
   <div class="jumbotron">
-<h1>Welcome!</h1><h2>To GC06 Team 21 Peer Assesment</h2>
+<h1>Welcome!</h1><h2>To GC06 - Team 21 Peer Assesment</h2>
   </div>
 </div>
 <?php
