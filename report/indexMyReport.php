512ec3c3f044ac3fb2a81695edfd27e48837ba1b
kartik <kartik.joshi.14@ucl.ac.uk>
2015-03-13 16:34:56 +0000

    moved some functions in report to helperFunctions.php

--- report/indexMyReport.php
+++ report/indexMyReport.php
@@ -1,5 +1,6 @@
 <?php
 	include "../navbar/navbar.php";
+  include "rankingFunctions.php";
 ?>
    <h1>My Report</h1>
  
@@ -67,36 +68,7 @@ echo $newReport->getReview2();
 <h1>Assesments</h1>
 <h2>Ranking - 
   <?php
-    function averageMark($group){
-      //Notes - This function returns the average mark recieved by the given group. Argument is groupID of group in question.
-      $conn = connectToDb();
-      $conn->select_db("team21");
-      $query = "SELECT reportID FROM reports WHERE group_ID = ".$group."";
-      $showResult = $conn->query($query);
-      $reportID = $showResult->fetch_array(MYSQLI_ASSOC);
-      $query = "SELECT assessments.assessmentID, assessments.comment, assessments.mark, assessments.criteria FROM assessments INNER JOIN groupreportassessment ON groupreportassessment.assessmentID = assessments.assessmentID WHERE groupreportassessment.report_ID = ".$reportID['reportID'].";";
-      $result = $conn->query($query);
-      while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
-        $_comment[]=$row['comment'];
-        $_mark[]=$row['mark'];
-        $_criteria[]=$row['criteria'];
-      }
-      $_average_mark = array_sum($_mark)/sizeof($_mark);
-      return $_average_mark;
-    }
-    function ranking(){
-      //Notes - This function will return a sorted array of groups and their average marks. It is sorted in
-      //        descending order of marks. So the position of the group in the array will be the rank. 
-      $conn = connectToDb();
-      $conn->select_db("team21");
-      $query = "SELECT group_ID FROM `reports`;";
-      $result = $conn->query($query);
-      while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
-        $averageMarkArray[$row['group_ID']] = averageMark($row['group_ID']);
-      }
-      arsort($averageMarkArray);
-      return $averageMarkArray;
-    }
+    
     $groupRanking = ranking();
     echo array_search($_SESSION['group'],array_keys($groupRanking))." out of ".sizeof($groupRanking)." groups."."\r\n";
     $groupID = $_SESSION['group'];
