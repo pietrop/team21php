6c938e4478d7ecf8fdff09e6a939e5aefb5f82dc
Asset <achita.n@me.com>
2015-03-18 17:21:23 +0000

    Updated sql script

--- TEST_FILES/createEmptyDB.sql
+++ TEST_FILES/createEmptyDB.sql
@@ -1,14 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.10
+-- version 4.2.8
 -- http://www.phpmyadmin.net
 --
--- Host: localhost:3306
--- Generation Time: Mar 11, 2015 at 04:50 PM
--- Server version: 5.5.38
--- PHP Version: 5.6.2
+-- Host: 127.0.0.1
+-- Generation Time: Mar 18, 2015 at 05:18 PM
+-- Server version: 5.6.20
+-- PHP Version: 5.6.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `team21`
@@ -22,45 +28,41 @@ USE `team21`;
 -- Table structure for table `admins`
 --
 
-CREATE TABLE `admins` (
+DROP TABLE IF EXISTS `admins`;
+CREATE TABLE IF NOT EXISTS `admins` (
   `email` varchar(320) NOT NULL,
   `firstName` varchar(160) DEFAULT NULL,
   `lastName` varchar(160) DEFAULT NULL,
   `password` varchar(320) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `admins`
---
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `assessments`
 --
 
-CREATE TABLE `assessments` (
+DROP TABLE IF EXISTS `assessments`;
+CREATE TABLE IF NOT EXISTS `assessments` (
   `assessmentID` int(100) NOT NULL,
   `criteria` varchar(100) NOT NULL,
   `mark` int(100) DEFAULT NULL,
   `comment` text
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `assessments`
---
-
 -- --------------------------------------------------------
 
 --
--- Table structure for table `forum`
+-- Table structure for table `Forum`
 --
 
-CREATE TABLE `forum` (
+DROP TABLE IF EXISTS `Forum`;
+CREATE TABLE IF NOT EXISTS `Forum` (
 `postID` int(11) NOT NULL,
-  `student_ID` varchar(50) NOT NULL,
+  `student_ID` varchar(40) NOT NULL,
   `parentPost_ID` int(11) DEFAULT NULL,
   `post` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -68,15 +70,12 @@ CREATE TABLE `forum` (
 -- Table structure for table `groupreportassessment`
 --
 
-CREATE TABLE `groupreportassessment` (
-`assessmentID` int(11) NOT NULL,
-  `group_ID` int(255) NOT NULL,
-  `report_ID` int(255) NOT NULL
+DROP TABLE IF EXISTS `groupreportassessment`;
+CREATE TABLE IF NOT EXISTS `groupreportassessment` (
+`assessmentID` int(100) unsigned NOT NULL COMMENT 'we need to find a way to increase the max possible size of this column. ',
+  `group_ID` int(99) DEFAULT NULL,
+  `report_ID` int(99) DEFAULT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `groupreportassessment`
---
 
 -- --------------------------------------------------------
 
@@ -84,14 +83,11 @@ CREATE TABLE `groupreportassessment` (
 -- Table structure for table `groups`
 --
 
-CREATE TABLE `groups` (
+DROP TABLE IF EXISTS `groups`;
+CREATE TABLE IF NOT EXISTS `groups` (
   `groupID` int(99) NOT NULL,
   `student_ID` varchar(320) NOT NULL DEFAULT ''
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `groups`
---
 
 -- --------------------------------------------------------
 
@@ -99,7 +95,8 @@ CREATE TABLE `groups` (
 -- Table structure for table `reports`
 --
 
-CREATE TABLE `reports` (
+DROP TABLE IF EXISTS `reports`;
+CREATE TABLE IF NOT EXISTS `reports` (
 `reportID` int(99) NOT NULL,
   `group_ID` int(99) NOT NULL COMMENT 'foreign key',
   `abstract` text,
@@ -107,26 +104,40 @@ CREATE TABLE `reports` (
   `review2` text
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `reports`
---
-
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `students`
 --
 
-CREATE TABLE `students` (
+DROP TABLE IF EXISTS `students`;
+CREATE TABLE IF NOT EXISTS `students` (
   `email` varchar(320) NOT NULL DEFAULT '',
   `firstName` varchar(160) DEFAULT NULL,
   `lastName` varchar(160) DEFAULT NULL,
   `password` varchar(160) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `students`
+-- Stand-in structure for view `studentswithgroupid`
 --
+DROP VIEW IF EXISTS `studentswithgroupid`;
+CREATE TABLE IF NOT EXISTS `studentswithgroupid` (
+`email` varchar(320)
+,`firstName` varchar(160)
+,`lastName` varchar(160)
+,`groupID` int(99)
+);
+-- --------------------------------------------------------
+
+--
+-- Structure for view `studentswithgroupid`
+--
+DROP TABLE IF EXISTS `studentswithgroupid`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `studentswithgroupid` AS select `students`.`email` AS `email`,`students`.`firstName` AS `firstName`,`students`.`lastName` AS `lastName`,`groups`.`groupID` AS `groupID` from (`students` left join `groups` on((`students`.`email` = `groups`.`student_ID`)));
 
 --
 -- Indexes for dumped tables
@@ -145,9 +156,9 @@ ALTER TABLE `assessments`
  ADD PRIMARY KEY (`assessmentID`,`criteria`);
 
 --
--- Indexes for table `forum`
+-- Indexes for table `Forum`
 --
-ALTER TABLE `forum`
+ALTER TABLE `Forum`
  ADD PRIMARY KEY (`postID`);
 
 --
@@ -179,17 +190,20 @@ ALTER TABLE `students`
 --
 
 --
--- AUTO_INCREMENT for table `forum`
+-- AUTO_INCREMENT for table `Forum`
 --
-ALTER TABLE `forum`
-MODIFY `postID` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `Forum`
+MODIFY `postID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `groupreportassessment`
 --
 ALTER TABLE `groupreportassessment`
-MODIFY `assessmentID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
+MODIFY `assessmentID` int(100) unsigned NOT NULL AUTO_INCREMENT COMMENT 'we need to find a way to increase the max possible size of this column. ',AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT for table `reports`
 --
 ALTER TABLE `reports`
 MODIFY `reportID` int(99) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
