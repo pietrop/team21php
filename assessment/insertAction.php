ea5d9aae9c7c64a616eb30635197aa3141142b5f
Asset <achita.n@me.com>
2015-03-18 11:53:02 +0000

    insertAction cleaned

--- assessment/insertAction.php
+++ assessment/insertAction.php
@@ -1,37 +1,17 @@
  <?php
-      include "assessment.php";
-      //assessment
-       $assessment = new assessment($_POST['assessmentID'], $_POST['criteria'], $_POST['mark'], $_POST['comment']);
-         // echo $assessment->getCriteria();
-       print_r($assessment);
+	include "assessment.php";
+	//assessment
+	$assessment = new assessment($_POST['assessmentID'], $_POST['criteria'], $_POST['mark'], $_POST['comment']);
+	//****DATABASE CONNECTION
+	$conn = connectToDb();
+	$conn->select_db("team21");
+	//****END OF CONNECTION PROCEDURE****
+	
+	$query = $assessment->createInsertQuery();
+	$conn->query($query);
+?>
 
-  
-
-     
-      //Database related information
-      $hostname="127.0.0.1";
-      $user="root";
-      $password="root";
-
-      $conn = new mysqli($hostname,$user,$password);
-      // Check connection
-      if ($conn->connect_error) {
-          die("Connection failed: " . $conn->connect_error);
-      } else {
-        echo "Connection successful<br>";
-      }
-
-      $myDB = $conn->select_db("team21");
-
-      //$query = 'INSERT INTO admins(email, firstName, lastName, password) VALUES ("'.$email.'","b","c","d")';
-      	$query = $assessment->createInsertQuery();
-      	$conn->query($query);
-
-
-      	
- ?>
-
-	<!-- redirects using js -->
-      <script>
-           location.href = "../report/myReportsToAssess.php";
-  </script>
+<!-- redirects using js -->
+<script>
+	location.href = "../report/myReportsToAssess.php";
+</script>
